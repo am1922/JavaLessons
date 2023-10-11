c91aaa2eac84e4e160e0fffa2cd20e67f3936641
Artem Mikhaylov <m.av1922@yandex.ru>
2023-10-11 22:10:24 +0300

    solved task_2.java

---
+++ task_2.java
@@ -0,0 +1,35 @@
+package Homeworks;
+
+public class task_2 {
+    public static void main(String[] args) {
+        int n = 1000;
+
+        System.out.println("Простые числа от 1 до " + n + ":");
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
